2914d3b52beeb3482479bba5dd68b2f99556bee0
Shubham Goel <ass@gmail.com>
2024-11-16 13:09:42 +0530

    search string

--- src/extension.ts
+++ src/extension.ts
@@ -156,7 +156,7 @@ export function activate(context: vscode.ExtensionContext) {
 		terminal.show();
 
 		saveTerminalOutput(terminal);
-		const searchString = 'debugmate>';  // String to search for
+		const searchString = 'debugMate>';  // String to search for
 		// processFile(cur_file, searchString);
 		
 		terminal.sendText('git diff > ' + GIT_DIFF_FILE);
